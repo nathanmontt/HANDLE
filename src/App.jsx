ee296377dab7acf2e7079e90ef7596d83738a230
nathanmontt <contato.mont@gmail.com>
2025-07-25 21:35:42 -0300

    Adding tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css'
+import Header from './assets/components/Header.jsx'
 
 function App() {
 
   return (
     <>
+      <Header />
     </>
   )
 }
---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+
+  return (
+    <>
+        <h2>Testing</h2>
+    </>
+  )
+}
+
+export default Header
