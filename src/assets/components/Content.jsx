881c210890737881f05c8faf8514e465f352ffc4
nathanmontt <contato.mont@gmail.com>
2025-07-27 15:40:07 -0300

    Timeout
    Adding a automatic image carrousel

--- src/assets/components/Content.jsx
+++ src/assets/components/Content.jsx
@@ -1,29 +1,32 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from 'react'
 
 function Content() {
-  const imagesArray = [
+  const images = [
     'src/assets/images/person-1.jpg',
     'src/assets/images/person-2.jpg',
     'src/assets/images/person-3.jpg',
-    'src/assets/images/person-4.jpg'
-  ];
+    'src/assets/images/person-4.jpg',
+  ]
 
-  const [currentIndex, setCurrentndex] = useState(0);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0)
+
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentndex((prevIndex) => {
-        prevIndex === imagesArray.length - 1 ? 0 : prevIndex + 1;
-      })
-    }, 1000);
+    const timeout = setTimeout(() => {
+      setCurrentImageIndex((prevIndex) => 
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      )
+    }, 3000);
 
-    return () => clearInterval(interval);
-  }, [imagesArray.length]);
+    // return () => clearTimeout(timeout)
+  }, [images.length])
 
   return (
     <main>
-      <img src={imagesArray[currentIndex]} alt="images" />
-
-      <img src="src/assets/images/person-1.jpg" alt="" />
+        <img
+          src={images[currentImageIndex]}
+          alt="Fashion Person"
+          className="people"
+        />
     </main>
   )
 }
